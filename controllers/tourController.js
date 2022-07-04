a2be294456e984fa43588201c7affee4d655c3aa
noobcoder-98 <anoobcoder98@gmail.com>
2022-07-05 02:17:39 +0700

    feat: add sorting, limiting fields, pagination, aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,16 @@
 const Tour = require('../models/tourModel')
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5'
+  req.query.sort = '-ratingsAverage,price'
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+  next()
+}
+
 exports.getAllTour = async (req, res) => {
   try {
     /** Build the query
-     * 1. Filtering
+     * 1A. Filtering
      */
     const queryObj = { ...req.query }
     const excludedFields = ['page', 'limit', 'sort', 'fields']
@@ -12,11 +19,40 @@ exports.getAllTour = async (req, res) => {
     })
 
     /**
-     * 2. Advanced filtering
+     * 1B. Advanced filtering
      */
-    const queryString = JSON.stringify(queryObj).replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-    const query = Tour.find(JSON.parse(queryString))
+    const queryString = JSON.stringify(queryObj).replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    )
+    let query = Tour.find(JSON.parse(queryString))
 
+    /** 2. Sorting */
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createAt')
+    }
+
+    /** 3. Field limiting */
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ')
+      query = query.select(fields)
+    } else {
+      query = query.select('-__v')
+    }
+
+    /** 4. Pagination */
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+    query = query.skip(skip).limit(10)
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments()
+      if (skip >= numTours) throw new Error('This page does not exist')
+    }
     /** Execute the query */
     const tours = await query
     res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,12 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTours
 } = require('../controllers/tourController')
 
 const router = express.Router()
 
+router.route('/top-5-cheap').get(aliasTopTours, getAllTour)
 router.route('/').get(getAllTour).post(createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
