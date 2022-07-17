384b5a8ddfd74f3c6fb89f7a3edd628fe387ae60
noobcoder-98 <anoobcoder98@gmail.com>
2022-07-17 23:43:56 +0700

    feat: building the tour overview

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
+const path = require('path')
 const express = require('express')
-
-const app = express()
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
@@ -11,9 +10,17 @@ const { globalErrorHandle } = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+const viewRouter = require('./routes/viewRoutes')
 const AppError = require('./utils/appError')
 
-// GLOBAL MIDDLEWARES
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+/** GLOBAL MIDDLEWARES */
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Set security HTTP headers
 app.use(helmet())
@@ -41,12 +48,18 @@ app.use(monggoSanitize())
 app.use(xss())
 
 // Prevent params pollution
-app.use(hpp({
-  whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
-}))
-
-// Serving static files
-app.use(express.static(`${__dirname}/public`))
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+)
 
 // Test middleware
 app.use((req, res, next) => {
@@ -54,6 +67,8 @@ app.use((req, res, next) => {
   next()
 })
 
+/** ROUTES */
+app.use('/', viewRouter)
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/reviews', reviewRouter)
---
+++ routes/viewRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { getTour } = require('../controllers/viewController')
+const { getOverview } = require('../controllers/viewController')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).render('base', {
+    title: 'haha'
+  })
+})
+
+router.get('/overview', getOverview)
+
+router.get('/tour', getTour)
+
+module.exports = router
