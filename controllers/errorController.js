0af8c7e6feada53d12a3d05dd580a1e778085f2d
noobcoder-98 <anoobcoder98@gmail.com>
2022-07-19 17:51:04 +0700

    feat: rendering error pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -19,23 +19,31 @@ const handleValidationErrorDB = (err) => {
 
 const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401)
 
-const handleJWTExpriredError = () => new AppError('Your token is exprired! Please log in again', 401)
+const handleJWTExpriredError = () =>
+  new AppError('Your token is exprired! Please log in again', 401)
 
 exports.globalErrorHandle = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
 
-  /** Operational, trusted error: send message to client */
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
+    // If error from backend
+    if (req.originalUrl.startsWith('/api')) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+      })
+    }
+    // Render website
+    res.status(err.statusCode).render('error', {
+      title: 'Something went wrong',
+      msg: err.message,
     })
-    /** Programming or other unknown error: don't leak error detail */
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err }
+    error.message = err.message
     if (error.name === 'CastError') {
       error = handleCastErrorDB(error)
     } else if (error.name === 11000) {
@@ -47,17 +55,31 @@ exports.globalErrorHandle = (err, req, res, next) => {
     } else if (error.name === 'TokenExpriredError') {
       error = handleJWTExpriredError()
     }
-    if (error.isOperational) {
-      res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message,
-      })
-    } else {
-      console.error('ERROR ', error)
+    /** Operational, trusted error: send message to client */
+    if (req.originalUrl.startsWith('/api')) {
+      if (error.isOperational) {
+        return res.status(error.statusCode).json({
+          status: error.status,
+          message: error.message,
+        })
+      }
+      /** Programming or other unknown error: don't leak error detail */
       res.status(500).json({
         status: 'error',
-        message: 'Something went wrong~',
+        message: 'Something went wrong!',
       })
     }
+    // Render website
+    if (error.isOperational) {
+      return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong',
+        msg: err.message,
+      })
+    }
+    /** Programming or other unknown error: don't leak error detail */
+    res.status(err.statusCode).render('error', {
+      title: 'Something went wrong',
+      msg: 'Please try again later!',
+    })
   }
 }
